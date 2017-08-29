2bc693620da0056a1f3f0fc78ee09a871bd44325
seifer8ff <brady.bcmp@gmail.com>
2017-08-28 18:20:44 -0700

    Responsive styling + added js comments
    Added media queries, and added comments to explain code.
    Still need to fix cursor placement on mobile.

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@
 	var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=dc6zaTOxFJmzC&limit=20&sort=recent";
 	var searchForm = document.querySelector("#form-search");
 	var queryInput = searchForm.querySelector("input");
+	var cursor = searchForm.querySelector("#cursor");
 	var queryLengthMin = 6;
 	var queryLengthMax = 20;
 
@@ -20,8 +21,8 @@
 
 		searchForm.addEventListener("submit", function(e) {
 			localStorage.setItem("keyword", queryInput.value);
-			//e.preventDefault();
 		});
+		// increase/decrease input size when typing
 		queryInput.addEventListener("keydown", function(e) {
 			if (queryInput.value.length + 4 < queryLengthMin) {
 				queryInput.size = queryLengthMin;
@@ -32,12 +33,19 @@
 				return;
 			}
 			queryInput.size = queryInput.value.length + 4;
-		})
+		});
+		// hide or display blinking cursor depending on input focus
+		queryInput.addEventListener("focus", function(e) {
+			cursor.style.display = "none";
+		});
+		queryInput.addEventListener("blur", function(e) {
+			cursor.style.display = "initial";
+		});
 	}
 	
 	
 	
-	
+	// get gifs and add them to DOM once gifs finish loading
 	XHR.makeRequest("GET", giphyURL)
 	.then(function(response) {
 		// load gifs

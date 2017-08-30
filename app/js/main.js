0bf17452af41b0d5628fcad7e161f4d93206e9aa
seifer8ff <brady.bcmp@gmail.com>
2017-08-30 12:32:18 -0700

    Reworked promise chain to load still imgs then gifs (faster loading)
    Now creating two imgs, one still and one gif.
    The stills get loaded first (using promise.all), then load the gifs,
    and swap display of the two imgs.

--- app/js/main.js
+++ app/js/main.js
@@ -18,60 +18,77 @@
 	function init() {
 		// get gifs and add them to DOM once gifs finish loading
 		XHR.makeRequest("GET", giphyURL)
-		.then(function(response) {
-			// load gifs
-			var gifs = JSON.parse(response);
-			var parentDiv = document.getElementById("gif-container");
-			var elements = [];
-		
-			for (var gifKey in gifs.data) {
-				if (!gifs.data.hasOwnProperty(gifKey)) {
-					continue;
-				}
-				var gif = gifs.data[gifKey];
-				// console.log(gif);
-				
-				// create gif wrapper div and add low quality gif as bg image
-				var div = document.createElement("div");
-				div.classList.add("gif-wrapper");
-				div.style.width = gif.images.fixed_width.width + "px";
-				div.style.height = gif.images.fixed_width.height + "px";
-				div.style.backgroundImage = "url(" + gif.images.fixed_width_still.url + ")";
-
-				// add hidden img and data attribute with url
-				var img = new Image(gif.images.fixed_width.width, gif.images.fixed_width.height);
-				img.classList.add("gif", "hidden");
-				img.dataset.src = gif.images.fixed_width.url;
-
-				//append to DOM and return element array 
-				div.appendChild(img);
-				parentDiv.appendChild(div);
-				elements.push(div);
-			}
-			return elements;
-		})
 		.catch(function(err) {
 			// error response from api
 			console.log("request error - status: " + err.status);
 			console.log(err);
 		})
-		.then(function(elements) {
-			// for each element, load the full gif
-			for (var i = 0; i < elements.length; i++) {
-				var parent = elements[i];
-				var gif = elements[i].childNodes[0];
+		.then(function(response) {
+			var gifs = JSON.parse(response);
+			var gifArray = [];
+		
+			for (var gifKey in gifs.data) {
+				if (!gifs.data.hasOwnProperty(gifKey)) {
+					continue;
+				}
+				gifArray.push(gifs.data[gifKey]);
+			}
+			return gifArray;
+		})
+		.then(function(gifs) {
+			return Promise.all(gifs.map(function (gif) {
+				return new Promise(function(resolve, reject) {
+					var parentDiv = document.getElementById("gif-container");
+					var div = document.createElement("div");
+					div.classList.add("gif-wrapper");
+	
+					var stillImg = new Image(gif.images.fixed_width.width, gif.images.fixed_width.height);
+					// set width and height to force positioning 
+					stillImg.style.width = gif.images.fixed_width.width + "px";
+					stillImg.style.height = gif.images.fixed_width.height + "px";
+					stillImg.classList.add("gif");
+					
+					// create high quality img that loads after all low quality imgs are done
+					var gifImg = new Image(gif.images.fixed_width.width, gif.images.fixed_width.height);
+					gifImg.classList.add("gif", "hidden");
+					gifImg.dataset.src = gif.images.fixed_width.url;
+					
+					// only move on once ALL low quality imgs done loading
+					// must use bind because function is asynchronous
+					stillImg.onload = function(parent) {
+						// remove style to allow for mobile styles to take over
+						this.removeAttribute("style");
+						resolve(parent);
+					}.bind(stillImg, div);
+	
+					div.appendChild(stillImg);
+					div.appendChild(gifImg);
+					parentDiv.appendChild(div);
 
-				// must use bind because it doesn't invoke function immediately
-				// unhide img + remove bg image of wrapper div. Remove height and width styling for mobile
-				gif.onload = function(parent) {
+					// set src to trigger the onload function above (which resolved promise)
+					stillImg.src = gif.images.fixed_width_still.url;
+				})
+			}));
+		})
+		.then(function(elements) {
+			return Promise.all(elements.map(function (el) {
+				var parent = el;
+				var stillImg = el.childNodes[0];
+				var gif = el.childNodes[1];
+
+				// swap low qual img with high qual
+				gif.onload = function(parent, stillImg) {
 					this.classList.remove("hidden");
-					parent.style.backgroundImage = "none";
-					parent.removeAttribute("style");
-				}.bind(gif, parent);
+					stillImg.classList.add("hidden");
+				}.bind(gif, parent, stillImg);
 
 				// img src must be set after onload event registered
 				gif.src = gif.dataset.src;
-			}
+			}));
+		})
+		.catch(function(err) {
+			// error somewhere in the promise chain
+			console.log(err);
 		})
 
 		addEventListeners();

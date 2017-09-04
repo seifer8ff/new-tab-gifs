e3be407d607231439c7f94f701d7d22997a49ee9
seifer8ff <brady.bcmp@gmail.com>
2017-09-03 17:21:47 -0700

    Refactored code + DOM fragment + reduced GIF quality
    Moved to appending to DOM fragment instead of directly to gif container
    to improve performance.
    Reduced still img + GIF quality to speed up loading.
    Refactored promise chain to break things up into functions + improve
    readability.
    Added better support for high res screens.

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
 (function() {
+
+	var settings = {
+		gifContainer: document.getElementById("gif-container"),
+		gifFragment: document.createDocumentFragment(),
+		limit: 30
+	} 
 	
 	init();
 
@@ -12,34 +18,11 @@
 		// get GIFs, add to GIF stills to DOM, and swap still imgs with gifs
 		getGIFs()
 		.then(function(gifs) {
-			return Promise.all(gifs.map(function (gif) {
-				return new Promise(function(resolve, reject) {
-					var parentDiv = document.getElementById("gif-container");
-					var div = document.createElement("div");
-					div.classList.add("gif-wrapper");
-	
-					var stillImg = new Image(gif.images.fixed_width.width, gif.images.fixed_width.height);
-					stillImg.classList.add("still", "hidden");
-					
-					// create high quality img that loads after all low quality imgs are done
-					var gifImg = new Image(gif.images.fixed_width.width, gif.images.fixed_width.height);
-					gifImg.classList.add("gif", "hidden");
-					gifImg.dataset.src = gif.images.fixed_width.url;
-					
-					// only move on once ALL low quality imgs done loading
-					// must use bind because function is asynchronous
-					stillImg.onload = function(parent) {
-						resolve(parent);
-					}.bind(stillImg, div);
-	
-					div.appendChild(stillImg);
-					div.appendChild(gifImg);
-					parentDiv.appendChild(div);
-
-					// set src to trigger the onload function above (which resolved promise)
-					stillImg.src = gif.images.fixed_width_still.url;
-				})
-			}));
+			return Promise.all(gifs.map(addGIFToFragment));
+		})
+		.then(function(elements) {
+			settings.gifContainer.appendChild(settings.gifFragment);
+			return elements;
 		})
 		.then(function(elements) {
 			// once all low quality images have loaded, unhide them
@@ -84,6 +67,7 @@
 					return resolve(gifObj.gifs);
 				}
 			} 
+
 			// only make an api request if gifs in localStorage are expired
 			XHR.makeRequest("GET", buildRequestURL())
 			.catch(function(err) {
@@ -92,17 +76,27 @@
 				console.log(err);
 			})
 			.then(function(response) {
-				var gifRes = JSON.parse(response);
-				var gifObj = {
-					gifs: [],
-					expires: new Date().getTime() + 60 * 20 * 1000
-				};
+				return JSON.parse(response);
+			})
+			.catch(function(err) {
+				console.log("error parsing response");
+			})
+			.then(function(response) {
+				console.log(response);
+				var gifArray = [];
 			
-				for (var gifKey in gifRes.data) {
-					if (!gifRes.data.hasOwnProperty(gifKey)) {
+				for (var gifKey in response.data) {
+					if (!response.data.hasOwnProperty(gifKey)) {
 						continue;
 					}
-					gifObj.gifs.push(gifRes.data[gifKey]);
+					gifArray.push(response.data[gifKey]);
+				}
+				return gifArray;
+			})
+			.then(function(gifArray) {
+				var gifObj = {
+					gifs: gifArray,
+					expires: new Date().getTime() + 60 * 20 * 1000
 				}
 				localStorage.setItem("gifObj", JSON.stringify(gifObj));
 				return resolve(gifObj.gifs);
@@ -110,18 +104,45 @@
 		});
 	}
 
+	function addGIFToFragment(gif) {
+		return new Promise(function(resolve, reject) {
+			var div = document.createElement("div");
+			div.classList.add("gif-wrapper");
+
+			var stillImg = new Image(gif.images.fixed_width_downsampled.width, gif.images.fixed_width_downsampled.height);
+			stillImg.classList.add("still", "hidden");
+			
+			// create high quality img that loads after all low quality imgs are done
+			var gifImg = new Image(gif.images.fixed_width_downsampled.width, gif.images.fixed_width_downsampled.height);
+			gifImg.classList.add("gif", "hidden");
+			gifImg.dataset.src = gif.images.fixed_width_downsampled.url;
+			
+			// only move on once ALL low quality imgs done loading
+			// must use bind because function is asynchronous
+			stillImg.onload = function(parent) {
+				resolve(parent);
+			}.bind(stillImg, div);
+
+			// set src to trigger the onload function above (which resolved promise)
+			stillImg.src = gif.images.fixed_width_small_still.url;
+
+			settings.gifFragment.appendChild(div);
+			div.appendChild(stillImg);
+			div.appendChild(gifImg);
+		})
+	}
+
 	function buildRequestURL() {
-		var limit;
 		// using screen.width because the browser can be resized to max
 		if (screen.width < 500 ) {
-			limit = 10;
+			settings.limit = 10;
 		} else if (screen.width < 1000) {
-			limit = 20;
+			settings.limit = 20;
 		} else {
-			limit = 30;
+			settings.limit = 30;
 		}
 
-		return "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=" + limit + "&sort=recent";
+		return "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=" + settings.limit + "&rating=g&sort=recent";
 	}
 
 	function isExpired(date) {
